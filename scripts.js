0c06724bf6b99befb74395699df1e974fbcaa8ad
Marcelo <cello.ma16@gmail.com>
2022-04-20 20:33:54 -0300

    implementa css do formulário e validação de preenchimento do formulário

--- scripts.js
+++ scripts.js
@@ -1,1 +1,64 @@
-console.log('fala aí, marcelin :)')
+const API_URL = 'https://mock-api.driven.com.br/api/v6/buzzquizz';
+const RECURSO_QUIZZES_URL = '/quizzes'; //Aceita GET e POST
+
+
+const urlRegex = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm;
+
+/*
+    O quizz criado se refere ao quizz q está sendo criado
+    Ao longo dos formulários, ele será preenchido com as informaçoes
+    O objeto deve ter a seguinte estrutura:
+
+    title:texto
+    image:text(url)
+    questions:lista de questoes
+        title:texto
+        color:cor em forma hexadecimal
+        answers:lista de respostas
+            texto:texto
+            image:texto(url)
+            isCorrectAnswer:boolean
+    levels:lista de niveis
+        title:texto
+        image:texto(url)
+        text:texto
+        minValue:numero
+*/
+const quizzCriadoObj = {};
+
+function validarInfoBasicas(titulo, imageUrl, qtdPerguntas, qtdNiveis) {
+    if (tituloIsValid(titulo)
+        && urlIsValid(imageUrl)
+        && qtdPerguntasIsValid(qtdPerguntas)
+        && qtdNiveisIsValid(qtdNiveis)) {
+
+        //aqui vai renderizar a proxima etapa das perguntas
+    } else {
+        alert("Você preencheu os dados de forma errada, preencha novamente!");
+    }
+}
+
+//validações info basicas criaçao de quizz
+function tituloIsValid(titulo) {
+    if (titulo != null)
+        return true;
+    return false;
+}
+
+function urlIsValid(url) {
+    if (url.match(urlRegex))
+        return true;
+    return false;
+}
+
+function qtdPerguntasIsValid(qtdPerguntas) {
+    if (qtdPerguntas >= 3)
+        return true;
+    return false;
+}
+
+function qtdNiveisIsValid(qtdNiveis) {
+    if (qtdNiveis >= 2)
+        return true;
+    return false;
+}
