da5e9e3db89ff639a781f0bb58dd603fcec8098c
Marcelo <cello.ma16@gmail.com>
2022-04-21 23:31:33 -0300

    adiciona listagem de quizzes de funcoes para transiçao de tela ao clicar nos quizzes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ const API_URL = 'https://mock-api.driven.com.br/api/v6/buzzquizz';
 const RECURSO_QUIZZES_URL = '/quizzes'; //Aceita GET e POST
 
 
-
 //regex para garantir que o q foi passado é um url válido
 const urlRegex = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm;
 
@@ -17,7 +16,7 @@ const urlRegex = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm;
         title:texto
         color:cor em forma hexadecimal
         answers:lista de respostas
-            texto:texto
+            text:texto
             image:texto(url)
             isCorrectAnswer:boolean
     levels:lista de niveis
@@ -41,6 +40,65 @@ function validarInfoBasicas(titulo, imageUrl, qtdPerguntas, qtdNiveis) {
 }
 
 
+getQuizzes();
+//requisiçoes
+function getQuizzes() {
+    const promise = axios.get(API_URL + RECURSO_QUIZZES_URL);
+    promise.then((response) => {
+        const listQuizzes = response.data.map(quizzConstructor);
+
+        console.log(`LIST QUIZZES: ${listQuizzes}`);
+
+        renderListQuizzes(listQuizzes);
+    });
+
+    promise.then((error) => {
+        console.log(error);
+    });
+}
+
+
+
+//construtores recebem um mapa como parametro e retornam o objeto
+function quizzConstructor(quizzData) {
+    const quizz = {};
+    quizz.title = quizzData.title;
+    quizz.image = quizzData.image;
+    quizz.questions = quizzData.questions.map(questionConstructor);
+    quizz.levels = quizzData.levels.map(levelConstructor);
+
+    return quizz;
+}
+
+function questionConstructor(questionData) {
+    const question = {};
+    question.title = questionData.title;
+    question.color = questionData.color;
+    question.answers = questionData.answers.map(answerConstructor);
+
+    return question;
+}
+
+function answerConstructor(answerData) {
+    const answer = {};
+    answer.text = answerData.text;
+    answer.image = answerData.image;
+    answer.isCorrectAnswer = answerData.isCorrectAnswer;
+
+    return answer;
+}
+
+function levelConstructor(levelData) {
+    const level = {};
+
+    level.title = levelData.title;
+    level.image = levelData.image;
+    level.text = levelData.text;
+    level.minValue = levelData.minValue;
+
+    return level;
+}
+
 //finalização da criação do quizz
 function onTapBackHome() {
     console.log("Voltou home");
@@ -50,19 +108,47 @@ function onTapBackHome() {
 
 //Aqui recebe o quizz(index no array, id ou objeto) como parametro para renderizar na tela
 function onTapQuizz(quizz) {
+    console.log(quizz);
     console.log("Apertou quizz");
     //TODO: criar funcao de renderizar
+}
+
+function renderListQuizzes(quizzes) {
+    const quizzesList = `
+        <div>
+            <span class="title-text-field">Todos os quizzes</span>
+            <div class="lista-quizzes">
+            </div>
+        </div>
+    `;
+    document.querySelector("main").innerHTML = quizzesList;
+    quizzes.map(renderCardQuizzLista);
+}
+
+function renderCardQuizzCricao(quizz) {
+    const cardQuizz = `<div class="card-quizz criacao-card" onclick="onTapQuizz('${quizz}')">
+            <img src="${quizz.image}"
+                alt="Imagem de exibição do Quizz">
+            <div class="degrade-card-quizz"></div>
+            <span>${quizz.title}</span>
+        </div>`;
+    document.querySelector(".lista-quizzes").innerHTML += cardQuizz;
+}
+
+function renderCardQuizzLista(quizz) {
+    const cardQuizz = `<div class="card-quizz" onclick="onTapQuizz('${quizz}')">
+            <img src="${quizz.image}"
+                alt="Imagem de exibição do Quizz">
+            <div class="degrade-card-quizz"></div>
+            <span>${quizz.title}</span>
+        </div>`;
+    document.querySelector(".lista-quizzes").innerHTML += cardQuizz;
 }
 
 //Aqui recebe o objeto do quizz como parametro pra já renderizar com as informaçoes corretas
 function renderQuizzCreated(quizz) {
     const quizzToRender = `<div class="title-form">Seu quizz está pronto!</div>
-        <div class="card-quizz criacao-card" onclick="onTapQuizz()">
-            <img src="${quizz.image}"
-                alt="Imagem de exibição do Quizz">
-            <div class="degrade-card-quizz"></div>
-            <span>${quizz.title}</span>
-        </div>
+        ${renderCardQuizzCricao(quizz)}
         <button class="btn-criar" onclick="onTapQuizz()">Acessar Quizz</button>
         <span class="sub-text" onclick="onTapBackHome()">Voltar pra home</span>`;
 
